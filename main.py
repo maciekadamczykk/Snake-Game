6d0edf0154733988c711b8bd815af989e1a4e62b
Maciej Adamczyk <maciekadamczyk.opole28@gmail.com>
2024-11-08 20:58:59 +0100

    Creating a screen for a snake game

---
+++ main.py
@@ -0,0 +1,11 @@
+#Import turtle module
+from turtle import *
+#Create a variable "t" and assign it to the "Turtle()" class so we can use the turtle methods
+t = Turtle()
+#Create a title for the screen "Snake"
+t.screen.title("Snake")
+#Set the colour of the background
+t.screen.bgcolor("Black")
+#Make the screen visable
+t.screen.mainloop()
+
