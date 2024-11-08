1a0cf693d84a79d639851db0bc5efcfc22bc3ce7
Maciej Adamczyk <maciekadamczyk.opole28@gmail.com>
2024-11-08 23:09:00 +0100

    Creating a snake object

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-#Import turtle module
-from turtle import *
-#Create a variable "t" and assign it to the "Turtle()" class so we can use the turtle methods
-t = Turtle()
-#Create a title for the screen "Snake"
-t.screen.title("Snake")
-#Set the colour of the background
-t.screen.bgcolor("Black")
-#Make the screen visable
-t.screen.mainloop()
+#Import turtle class and screen 
+from turtle import Turtle, Screen
+#Create a screen
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Snake")
+screen.screensize(600,600)
 
-#asdf
+#Create an object which will be the square on the screen
+starting_points = [(0,0), (-20,0), (-40, 0)]
+
+parts = []
+
+for part in starting_points:
+    new_part = Turtle()
+    new_part.shape("square")
+    new_part.color("#2CFF05")
+    new_part.goto(part)
+    parts.append(new_part)
+
+
+
+screen.mainloop()
